0f16bf6f1b26e362c64621c3a0c1185412364d64
mazen <mazen.ali.rushdy@gmail.com>
2025-03-22 07:31:07 +0200

    Solve the object state challenge

--- src/App.js
+++ src/App.js
@@ -1,56 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
-import MyFirstComponent from "./MyFirstComponent";
-import Header from "./Header";
-import Post from "./Post";
-import SideMenu from "./SideMenu";
+import MyButton from "./MyButtns";
+import MyInput from "./myInput";
+import MyForm from "./MyForm";
+import Scoreboard from "./Scoreboard";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-
-      {/* POSTS CONTAINER */}
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <div
-          style={{ width: "60%", display: "flex", justifyContent: "center" }}
-        >
-          {/* == POSTS CONTAINER == */}
-          <div style={{ width: "70%" }}>
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-          </div>
-
-          <div style={{ width: "30%", marginTop: "25px" }}>
-            <SideMenu />
-          </div>
-        </div>
-      </div>
-
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to refresh.
-        </p>
-
-        <h1>Tarmeez Academy</h1>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
+    <Scoreboard />
   );
 }
+
 
 export default App;
--- src/TagButton.js
+++
@@ -1,4 +0,0 @@
-import "./TagButtonStyle.css";
-export default function TagButton() {
-  return <button className="tagButton">Tag Button</button>;
-}
