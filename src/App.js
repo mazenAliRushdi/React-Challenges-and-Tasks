69939ee113918771d0208945a82ead4ef9de38c2
mazen <mazen.ali.rushdy@gmail.com>
2025-03-23 17:10:30 +0200

    Solve the components challenge

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header";
+import Post from "./Post";
+import SideMenu from "./SideMenu";
 
-export default function App() {
+function App() {
   return (
-    <h1>
-      Hello World
-    </h1>
+    <div className="App">
+      <Header />
+
+      {/* POSTS CONTAINER */}
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div
+          style={{ width: "60%", display: "flex", justifyContent: "center" }}
+        >
+          {/* == POSTS CONTAINER == */}
+          <div style={{ width: "70%" }}>
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+          </div>
+
+          <div style={{ width: "30%", marginTop: "25px" }}>
+            <SideMenu />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
+
+export default App;
