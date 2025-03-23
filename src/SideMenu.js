02f594f5474c10f3ec6b409c38aaaea4b8e328c9
mazen <mazen.ali.rushdy@gmail.com>
2025-03-23 17:33:21 +0200

    Solve The List Rendering Challenge

--- src/SideMenu.js
+++ src/SideMenu.js
@@ -1,36 +1,53 @@
 import TagButton from "./TagButton";
 
 export default function SideMenu() {
-  return (
-    <div style={{ border: "solid teal 5px" }}>
-      <TagButton title="آخر المقالات">
+  const categories = [
+    {
+      uniqueId: 1,
+      title: "آخر المقالات",
+      c: (
         <div>
           <span>😀😀😀</span>
         </div>
-      </TagButton>
+      ),
+    },
 
-      <TagButton title="الأكثر قراءة">
+    {
+      uniqueId: 2,
+      title: "الأكثر قراءة",
+      c: (
         <div>
           <img
             style={{ width: "100px" }}
             src="https://www.thespruce.com/thmb/ClRANI4jTWhkGeNhvJtArRhlGSA=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/the-difference-between-trees-and-shrubs-3269804-hero-a4000090f0714f59a8ec6201ad250d90.jpg"
           />
         </div>
+      ),
+    },
+
+    {
+      uniqueId: 3,
+      title: "مميز",
+      c: (
+        <>
+          <h1>Click Me</h1>
+          <span>👍🏻</span>
+        </>
+      ),
+    },
+  ];
+
+  const categoriesList = categories.map((cat) => {
+    return (
+      <TagButton key={cat.uniqueId} title={cat.title}>
+        {cat.c}
       </TagButton>
-      <TagButton title="مميز">
-        <h1>Click Me</h1>
-        <span>😐😐</span>
-      </TagButton>
-      <TagButton title="مقالات الأسبوع" />
-      {/* <TagButton />
-      <TagButton />
-      <TagButton />
-      <TagButton />
-      <TagButton />
-      <TagButton />
-      <TagButton />
-      <TagButton />
-      <TagButton /> */}
+    );
+  });
+
+  return (
+    <div style={{ border: "solid teal 5px" }}>
+      {categoriesList}
     </div>
   );
 }
