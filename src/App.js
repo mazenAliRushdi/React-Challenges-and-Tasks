f30eb34fcb1930261bcf75521e216c010f583916
mazen <mazen.ali.rushdy@gmail.com>
2025-03-24 06:40:49 +0200

    Solve the successive state update challenge

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 export default function App() {
+  const [pending, setPending] = useState(0);
+  const [completed, setCompleted] = useState(0);
+
+  async function handleClick() {
+    setPending(pending + 1);
+    await delay(3000);
+    setPending((p) => {
+      return p - 1;
+    });
+    setCompleted((c) => {
+      return c + 1;
+    });
+  }
+
   return (
-    <h1>
-      Hello World
-    </h1>
+    <>
+      <h3>
+        Pending: {pending}
+      </h3>
+      <h3>
+        Completed: {completed}
+      </h3>
+      <button onClick={handleClick}>
+        Buy     
+      </button>
+    </>
   );
+}
+
+function delay(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
 }
