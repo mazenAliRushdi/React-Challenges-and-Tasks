b105b029f8d5e5fd6f991ddfe7b8c1d180014c19
mazen <mazen.ali.rushdy@gmail.com>
2025-03-30 13:20:05 +0200

    Solve the context challenge

--- src/App.js
+++ src/App.js
@@ -1,10 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { places } from './data.js';
+import { getImageUrl } from './utils.js';
+import { imageSizeContext, placeContext } from './Context.js';
+import { useContext } from 'react';
+
 
 export default function App() {
+  const [isLarge, setIsLarge] = useState(false);
+  const imageSize = isLarge ? 150 : 100;
+
+    return (
+      <imageSizeContext.Provider value={imageSize}>
+        <label>
+          <input
+            type="checkbox"
+            checked={isLarge}
+            onChange={e => {
+              setIsLarge(e.target.checked);
+            }}
+          />
+          Use large images
+        </label>
+        <hr />
+        <List />
+      </imageSizeContext.Provider>
+    )
+}
+
+
+function List() {
+  const listItems = places.map(place =>
+    <placeContext.Provider value={place}>
+      <li key={place.id}>
+        <Place />
+      </li>
+    </placeContext.Provider>
+  );
+  return <ul>{listItems}</ul>;
+}
+
+function Place() {
+  const place = useContext(placeContext);
   return (
-    <h1>
-      Hello World
-    </h1>
+    <>
+      <PlaceImage />
+      <p>
+        <b>{place.name}</b>
+        {': ' + place.description}
+      </p>
+    </>
+  );
+}
+
+function PlaceImage() {
+  const imageSize = useContext(imageSizeContext);
+  const place = useContext(placeContext);
+  return (
+    <img
+      src={getImageUrl(place)}
+      alt={place.name}
+      width={imageSize}
+      height={imageSize}
+    />
   );
 }
---
+++ src/Context.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+export const imageSizeContext = createContext();
+
+export const placeContext = createContext();
